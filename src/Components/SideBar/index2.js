874629392bcad997be85e57234a3733767a1e31c
Diksharayat <rayatdiksha787@gmail.com>
2024-05-23 13:11:35 +0530

    sidebar link

--- src/Components/SideBar/index2.js
+++ src/Components/SideBar/index2.js
@@ -84,7 +84,7 @@ export default function PersistentDrawerLeft(props) {
 
   const fetchCartItemsCount = async () => {
     try {
-      const response = await axios.get('http://localhost:10000/api/cart');
+      const response = await axios.get('https://mcd-pi.vercel.app/cart');
       const cartItems = response.data.data;
       const count = cartItems.length; 
       setCartItemsCount(count);
